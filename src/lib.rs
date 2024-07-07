4be8825277a44015003473844cec10287ab448dd
dari-os <Darius.dev@proton.me>
2024-07-07 13:30:19 +0200

    Added the decoding function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 mod preprocessing;
 
-use preprocessing::{PREPROCESSED_ARRAY, HEX_DIGITS};
+use preprocessing::{HEX_DIGITS, PREPROCESSED_ARRAY};
 
-
-pub fn encode(str_to_encode: &str) -> String  {
+pub fn encode(str_to_encode: &str) -> String {
     let mut encoded_string = String::with_capacity(str_to_encode.len() * 3);
 
     for &current_byte in str_to_encode.as_bytes() {
@@ -19,14 +18,48 @@ pub fn encode(str_to_encode: &str) -> String  {
     return encoded_string;
 }
 
-pub fn decode(_str_to_decode: &str) -> Option<String>{
-    todo!()
+//TODO: Add multi-byte utf8 support
+pub fn decode(str_to_decode: &str) -> String {
+    let mut decoded_string = String::with_capacity(str_to_decode.len());
+
+    let bytes = str_to_decode.as_bytes();
+
+    let mut i = 0;
+    while i < bytes.len() {
+        if let (Some(b'%'), Some(n1), Some(n2)) = (bytes.get(i), bytes.get(i + 1), bytes.get(i + 2))
+        {
+
+            let decoded_byte: Option<u8> = match (from_hex(*n1), from_hex(*n2)) {
+                (Some(n1), Some(n2)) => Some(n1 << 4 | n2),
+                _ => None,
+            };
+
+            if let Some(temp) = decoded_byte {
+                decoded_string.push(temp as char);
+                i += 3;
+                continue;
+            }
+        }
+
+        decoded_string.push(bytes[i] as char);
+        i += 1;
+    }
+    decoded_string
+}
+
+fn from_hex(c: u8) -> Option<u8> {
+    match c {
+        b'0'..=b'9' => Some(c - b'0'),
+        b'A'..=b'F' => Some(c - b'A' + 10),
+        b'a'..=b'f' => Some(c - b'a' + 10),
+        _ => None,
+    }
 }
 
 pub trait Encoder<T = String> {
     fn url_encode(&self) -> T;
 
-    fn url_decode(&self) -> Option<T>;
+    fn url_decode(&self) -> T;
 }
 
 impl Encoder for String {
@@ -34,7 +67,7 @@ impl Encoder for String {
         encode(self)
     }
 
-    fn url_decode(&self) -> Option<String> {
+    fn url_decode(&self) -> String {
         decode(self)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ extern crate url_encor;
 
 use url_encor::Encoder;
 fn main() {
-let test_string = String::from(r#"Hello !"&/?)"§$`)(="§`$=) €@"#);
-    println!("{}", test_string.url_encode())
+    let test_string = String::from(r#"Hello, World! "#);
+    let encoded_string = test_string.url_encode();
+    println!("{}", encoded_string);
+    //let decoded_string = encoded_string.url_decode();
+    let decoded_string = String::from("Hello %2G:");
+    println!("{}", decoded_string)
 }
